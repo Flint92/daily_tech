b50307c5582b0cfb382809903fa1e8db3ed10bbf
Flint92 <1992flint@gmail.com>
2025-08-31 20:27:04 +0800

    -- expire message

--- 20250816/hecto/src/component/messagebar.rs
+++ 20250816/hecto/src/component/messagebar.rs
@@ -1,19 +1,45 @@
 use crate::component::uicomponent::UIComponent;
 use crate::edit::terminal::{Size, Terminal};
 use std::io::Error;
+use std::time::{Duration, Instant};
+
+const DEFAULT_DURATION: Duration = Duration::new(5, 0);
+
+struct Message {
+    text: String,
+    time: Instant,
+}
+
+impl Default for Message {
+    fn default() -> Self {
+        Self {
+            text: String::new(),
+            time: Instant::now(),
+        }
+    }
+}
+
+impl Message {
+    fn is_expired(&self) -> bool {
+        Instant::now().duration_since(self.time) > DEFAULT_DURATION
+    }
+}
 
 #[derive(Default)]
 pub struct MessageBar {
-    current_message: String,
+    current_message: Message,
     needs_redraw: bool,
+    cleared_after_expiry: bool,
 }
 
 impl MessageBar {
     pub fn update_message(&mut self, new_message: String) {
-        if new_message != self.current_message {
-            self.current_message = new_message;
-            self.mark_redraw(true);
-        }
+        self.current_message = Message {
+            text: new_message,
+            time: Instant::now(),
+        };
+        self.cleared_after_expiry = false;
+        self.mark_redraw(true)
     }
 }
 
@@ -23,12 +49,25 @@ impl UIComponent for MessageBar {
     }
 
     fn needs_redraw(&self) -> bool {
-        self.needs_redraw
+        (!self.cleared_after_expiry && self.current_message.is_expired()) || self.needs_redraw
     }
 
     fn set_size(&mut self, _: Size) {}
 
     fn draw(&mut self, origin_y: usize) -> Result<(), Error> {
-        Terminal::print_row(origin_y, &self.current_message)
+        if self.current_message.is_expired() {
+            self.cleared_after_expiry = true;
+            // Upon expiration, we need to write out "" once to clear the message.
+            // To avoid clearing more than necessary,
+            // we  keep track of the fact that we've already cleared the expired message once.
+            // 1Code has comments. Press enter to view.
+        }
+        let message = if self.current_message.is_expired() {
+            ""
+        } else {
+            &self.current_message.text
+        };
+
+        Terminal::print_row(origin_y, message)
     }
 }
